9e7ba66178ba95f0e9c9d042047f4a9dd5e3047b
Hero <hero4barca@gmail.com>
2023-02-13 17:44:41 +0100

    added profile to view and urls

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django import forms
 from django.contrib.auth import get_user_model
 
@@ -17,3 +17,7 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ('email', 'username', 'first_name', 'last_name','password1', 'password2')
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(label='Email / Username')
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
 
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 
 # Create your views here.
 
 
+class CustomLoginView(LoginView):
+    form_class  = LoginForm
 
 def register(request):
     if request.method == 'POST':
@@ -20,3 +24,7 @@ def register(request):
 
     context = {'form': form}
     return render(request, 'registration/register.html', context)
+
+@login_required
+def home(request):
+    return render (request, 'accounts/home.html')
