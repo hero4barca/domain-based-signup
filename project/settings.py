1cc6bb8d5804d6ce9cb3758a0647798c313e1e0b
Hero <hero4barca@gmail.com>
2023-02-15 12:20:47 +0100

    added basic styles to html templates

--- project/settings.py
+++ project/settings.py
@@ -135,6 +135,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'project/static'),
+    
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
