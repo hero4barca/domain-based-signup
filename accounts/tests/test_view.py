9893cae6990389b254f931439a76ac4d0d73cb26
Hero <hero4barca@gmail.com>
2023-02-16 13:03:46 +0100

    updated view_test: test_signup_with_whitelisted_domain_successful

--- accounts/tests/test_view.py
+++ accounts/tests/test_view.py
@@ -50,6 +50,30 @@ class SignupViewTest(TestCase):
         self.assertIsInstance(new_user, User, "New user was not created")
 
     
+    def test_signup_with_whitelisted_domain_successful(self):
+
+        whitelisted_domain  = WhitelistDomains.objects.create(
+            domain = "domain.com",
+            is_active = True
+        )
+        self.assertIn(whitelisted_domain, WhitelistDomains.objects.all())
+    
+        signup_form_data = {
+            "email": "test@domain.com",
+            "username": "user01",
+            "password1": "Password@1",
+            "password2": "Password@1"
+        }
+
+        response = self.client.post(reverse('signup'),
+                                     data=signup_form_data) 
+        self.assertEquals(response.status_code, 200 )
+
+        # # asser new user not created
+        new_user = User.objects.filter(email="test@domain.com").first()
+        self.assertIsInstance(new_user, User, "New user was not created")
+
+    
 
 
 
